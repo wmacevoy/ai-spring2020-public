9001a081de02763d0f65e2377aaf02053b003492
Warren MacEvoy <wmacevoy@gmail.com>
2020-02-11 15:35:45 -0800

    scared goat agent

--- baghchal/main.py
+++ baghchal/main.py
@@ -3,10 +3,12 @@ from game import Game
 import random
 from matchup import Matchup
 from hungrytigeragent import HungryTigerAgent
+from scaredgoatagent import ScaredGoatAgent
 from stats import Stats
 
 matchup = Matchup()
 # matchup.tigerAgent = HungryTigerAgent(matchup.game)
-stats = Stats(matchup, 1000)
+matchup.goatAgent = ScaredGoatAgent(matchup.game)
+stats = Stats(matchup, 100)
 stats.playAll()
 stats.summarize()
