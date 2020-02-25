a134a0567373866d72964fde5934e05bd0f4ad72
Warren MacEvoy <wmacevoy@gmail.com>
2020-02-25 15:00:24 -0800

    playoff

---
+++ baghchal/playoff.py
@@ -0,0 +1,33 @@
+from typing import Dict
+from agent import Agent
+from matchup import Matchup
+from stats import Stats
+
+class Playoff:
+    def __init__(self, trials : int = 10):
+        self._goatAgents : Dict[str,Agent] = dict()
+        self._tigerAgents : Dict[str,Agent] = dict()
+        self._trials : int = trials
+
+    def addGoatAgent(self,name : str,agent : Agent) -> None:
+        self._goatAgents[name]=agent
+
+    def addTigerAgent(self,name : str,agent : Agent) -> None:
+        self._tigerAgents[name]=agent
+
+    def play(self):
+        for goatAgentName in self._goatAgents:
+            for tigerAgentName in self._tigerAgents:
+                goatAgent : Agent = self._goatAgents[goatAgentName]
+                tigerAgent : Agent = self._tigerAgents[tigerAgentName]
+                matchup : Matchup = Matchup()
+                matchup.tigerAgent = tigerAgent
+                matchup.goatAgent = goatAgent
+                stats : Stats = Stats(matchup,10)
+                stats.playAll()
+                print(f"goat {goatAgentName} vs. tiger {tigerAgentName}: ")
+                stats.summarize()
+
+
+
+    
